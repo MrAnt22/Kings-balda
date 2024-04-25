2606306244f7ed3147b7406fe26b2139acc9dcd9
redhauser <redhauser@gmail.com>
2024-04-25 17:08:20 +0300

    добавив перевірку поля горизонтально

--- main.cpp
+++ main.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
+#include <set>
 
 #define SIZE 5
+#define GOD_WORD "GOD"
 
 using namespace std;
 
 void displayField();
-void writeToField(char inp[64]);
+bool writeToField(string inp);
+void mainLoop(string c);
+void checkField();
 
 char field[SIZE][SIZE];
+int score = 0;
+bool isFirstPlayer = true;
+set<string> foundWords;
 
 int main(){
     //initializaiton
     for(int i = 0; i < SIZE; i++) {
         for(int j = 0; j < SIZE; j++) {
-            field[i][j] = i*j+48;
+            field[j][i] = ' ';
         }
+    }
+
+    for(int i = 0; i < SIZE-2; i++) {
+        field[0][i] = GOD_WORD[i-0];
     }
     
     //main loop
-    //cout << "dumb mafuacka" << endl ;
-    char c[64];
+    string c;
     while(c[0] != '!') {
+        mainLoop(c);
+    }
+}
+
+void mainLoop(string c) {
         displayField();
-        cout << "Enter field pos and char: [3a=X]";
+        cout << "Your discovered words are: [";
+        for(auto word: foundWords) {
+            cout << word << ", ";
+        }
+        cout << "]" << endl;
+        cout << "<Your score: {" << foundWords.size() << "}>"<<endl;
+        cout << "Enter field pos and char [3A=X]:";
         cin >> c;
         cout << "\x1B[2J\x1B[H";
-        writeToField(c);
-    }
+       if(!writeToField(c)) {
+            mainLoop(c);
+       }
+       checkField();
 }
 
 /*
@@ -34,12 +57,22 @@ int main(){
 
 */
 
-void writeToField(char inp[64]) {
+bool writeToField(string inp) {
     int x = inp[0] - 49;
     int y = inp[1] - 65;
     char val = inp[3];
+
+    if(x < 0 || x > SIZE || y < 0 || y > SIZE) {
+        return false;
+    }
+
+    if(((field[x-1][y] == ' ' || field[x-1][y] == 0) && (field[x+1][y] == ' ' || field[x+1][y] == 0)) && ((field[x][y-1] == ' ' || field[x][y-1] == 0) && (field[x][y+1] == ' ' || field[x][y+1] == 0))) {
+        return false;
+    }
+
     //cout << x << " " << y  << " " << val << endl;
     field[x][y] = val;
+    return true;
 }
 
 void displayField() {
@@ -55,4 +88,27 @@ void displayField() {
         }
     }
     cout << "┗━━━┻━━━┻━━━┻━━━┻━━━┛" << endl;
+}
+
+void checkField() {
+    bool isWord = true;
+
+    int length = 3;
+
+    for(int t = 0; t < 3; t++) {
+        isWord = true;
+        for(int i = 0; i < SIZE; i++) {
+            for(int j = 0+t; j < SIZE; j++) {
+                if(field[i][j] == GOD_WORD[j-t] && isWord) {
+                    isWord = true;
+                } else {
+                    isWord = false;
+                }
+                if(j == SIZE-3+t && isWord) {
+                    foundWords.insert(GOD_WORD);
+                }
+            }
+        }
+    }
+    
 }
