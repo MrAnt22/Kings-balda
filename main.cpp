1430c9da585565e231a9a4da46495666b56ef18e
MrAnt22 <makslipovyi@gmail.com>
2024-05-08 19:23:28 +0300

    classi1

--- main.cpp
+++ main.cpp
@@ -8,130 +8,182 @@
 
 using namespace std;
 
-void displayField();
-bool writeToField(string inp);
-void mainLoop(string c);
-void checkField();
+class Coordinates{
+public:
+    int x;
+    int y;
+    char symbol;
+    int move[10];
+};
 
-char field[SIZE][SIZE];
-int score = 0;
-bool isFirstPlayer = true;
-
-set<string> foundWords;
-set<string> allWords;
-
-int main() {
-    //DICTIONARY SHIT
-    string word;
-    ifstream dict("dictionary.txt");
-    while(getline(dict, word)) {
-        for (auto& x : word) {
-        x = toupper(x);
+class Board{
+    char field[SIZE][SIZE];
+    int size = SIZE;
+public:
+    void display(){
+        cout << "┏━A━┳━B━┳━C━┳━D━┳━E━┓" << endl;
+        for(int i = 0; i < size; i++) {
+            cout << (i+1) << " ";
+            for(int j = 0; j < size; j++) {
+                cout << field[i][j] << " ┃ ";
+            }
+            cout << endl;
+            if(i != size-1) {
+                cout << "┣━━━╋━━━╋━━━╋━━━╋━━━┫" << endl;
+            }
         }
-        allWords.insert(word);
+        cout << "┗━━━┻━━━┻━━━┻━━━┻━━━┛" << endl;  
     }
-    dict.close();
-
-    //FIELD AND SHIT
-    //initializaiton
-    for(int i = 0; i < SIZE; i++) {
-        for(int j = 0; j < SIZE; j++) {
-            field[j][i] = ' ';
+    bool place(Coordinates obj){
+        if(obj.x < 0 || obj.x > SIZE || obj.y < 0 || obj.y > SIZE) {
+            return false;
         }
-    }
-
-    for(int i = 0; i < SIZE-0; i++) {
-        field[2][i] = GOD_WORD[i-0];
-    }
-    
-    //main loop
-    string c;
-    while(c[0] != '!') {
-        mainLoop(c);
-    }
-}
-
-int yoScore(){
-    int sumScore = 0;
-    for(auto word: foundWords) {
-            //cout << word << ", ";
-            sumScore += word.length();
+        if(((field[obj.x-1][obj.y] == ' ' || field[obj.x-1][obj.y] == 0) && (field[obj.x+1][obj.y] == ' ' || field[obj.x+1][obj.y] == 0)) && ((field[obj.x][obj.y-1] == ' ' || field[obj.x][obj.y-1] == 0) && (field[obj.x][obj.y+1] == ' ' || field[obj.x][obj.y+1] == 0))) {
+            return false;
         }
-    return sumScore;
-}
-
-void mainLoop(string c) {
-        displayField();
-        cout << "Your discovered words are: [";
-        for(auto word: foundWords) {
-            cout << word << " " ;
-        }
-        cout << "]" << endl;
-        cout << "<Your score: {" << yoScore() << "}>"<<endl;
-        cout << "Enter field pos and char [3A=X]:";
-        cin >> c;
-        cout << "\x1B[2J\x1B[H";
-       if(!writeToField(c)) {
-            mainLoop(c);
-       }
-       checkField();
-}
-
-bool writeToField(string inp) {
-    int x = inp[0] - 49;
-    int y = inp[1] - 65;
-    char val = inp[3];
-
-    if(x < 0 || x > SIZE || y < 0 || y > SIZE) {
-        return false;
+        field[obj.x][obj.y] = obj.symbol;
+        return true;
     }
-    if(((field[x-1][y] == ' ' || field[x-1][y] == 0) && (field[x+1][y] == ' ' || field[x+1][y] == 0)) && ((field[x][y-1] == ' ' || field[x][y-1] == 0) && (field[x][y+1] == ' ' || field[x][y+1] == 0))) {
-        return false;
-    }
-
-    field[x][y] = val;
-    return true;
-}
-
-void displayField() {
-    cout << "┏━A━┳━B━┳━C━┳━D━┳━E━┓" << endl;
-    for(int i = 0; i < SIZE; i++) {
-        cout << (i+1) << " ";
-        for(int j = 0; j < SIZE; j++) {
-            cout << field[i][j] << " ┃ ";
-        }
-        cout << endl;
-        if(i != SIZE-1) {
-            cout << "┣━━━╋━━━╋━━━╋━━━╋━━━┫" << endl;
-        }
-    }
-    cout << "┗━━━┻━━━┻━━━┻━━━┻━━━┛" << endl;
-}
-
-void checkField() {
-    bool isWord = true;
-
-    int length;
-
-    //horizontal
-    for(int isVertical = 0;isVertical < 2; isVertical++){
-        for(int i = 0; i < SIZE; i++) {
-            for (string wrd: allWords) {
-                length = wrd.length();
-                for(int t = 0; t < (6-length); t++) {
-                    isWord = true;
-                    for(int j = 0+t; j < length+t; j++) {
-                        if(field[isVertical?j:i][isVertical?i:j] == wrd[j-t] && isWord) {
-                            isWord = true;
-                        } else {
-                            isWord = false;
-                        }
-                        if(j == SIZE-(6-length)+t && isWord) {
-                            foundWords.insert(wrd);
-                        }
-                    }
-                }
+    void clearBoard(){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                field[i][j] = ' ';
             }
         }
     }
+    void centerWord(string word){
+        for(int i = 0; i < size-0; i++) {
+        field[size/2][i] = word[i-0];
+        }
+    }
+};
+
+class Dictionary{
+public:
+    set<string> words;
+    set<string> userWords;
+    set<string> pcWords;
+    string filename = "dictionary.txt";
+    void loadDictionary(){
+        string word;
+        ifstream dict(filename);
+
+        while(getline(dict, word)) {
+            for (auto& x : word) {
+                x = toupper(x);
+            }
+            words.insert(word);
+        }
+        dict.close();
+    }
+    void introduceWord(string word){
+        words.insert(word);
+        ofstream MyFile(filename,ios::out | ios::app);
+        MyFile << word;
+        MyFile.close();
+    }
+};
+
+class Game{
+public:
+    bool isPlayerTurn;
+    void makeMove(){
+        //
+    }
+    Coordinates evaluateInput(string inp){
+        Coordinates obj;
+        obj.x = inp[0] - 49;
+        obj.y = inp[1] - 65;
+        obj.symbol = inp[3];
+        return obj;
+    }
+    int score(bool isUser,Dictionary dict){
+        int sumScore = 0;
+        for(auto word: (isUser?dict.userWords:dict.pcWords)) {                                                                                                                              //cout << word << ", ";
+                sumScore += word.length();
+            }
+        return sumScore;
+    }
+};
+
+int main() {
+    Dictionary dict;
+    dict.loadDictionary();
+    
+    Board board;
+    board.clearBoard();
+    board.centerWord(GOD_WORD);
+
+    Game game;
+    
+
+    string c;
+    dict.introduceWord("skibidi toilet");
+    while(c[0] != '!') {
+        board.display();
+        cout << "Your discovered words are: [";
+        for(auto word: dict.userWords) {
+            cout << word << " " ;
+        }
+        cout << "]" << endl;
+        cout << "<Your score: {" << game.score(true, dict) << "}>"<<endl;
+        cout << "Enter field pos and char [3A=X]:";
+        cin >> c;
+        cout << "\x1B[2J\x1B[H";
+    }
 }
+// int yoScore(){
+//     int sumScore = 0;
+//     for(auto word: foundWords) {
+//             //cout << word << ", ";
+//             sumScore += word.length();
+//         }
+//     return sumScore;
+// }
+
+//void mainLoop(string c, Board board, Dictionary dict, Game game){
+    //     board.display();
+    //     cout << "Your discovered words are: [";
+    //     for(auto word: dict.userWords) {
+    //         cout << word << " " ;
+    //     }
+    //     cout << "]" << endl;
+    //     cout << "<Your score: {" << yoScore() << "}>"<<endl;
+    //     cout << "Enter field pos and char [3A=X]:";
+    //     cin >> c;
+    //     cout << "\x1B[2J\x1B[H";
+    //    if(!writeToField(c)) {
+    //         mainLoop(c);
+    //    }
+    //    checkField();
+//}
+
+
+                                                                /*Hall of Fame*/
+// void checkField() {
+//     bool isWord = true;
+
+//     int length;
+
+    //horizontal
+//     for(int isVertical = 0;isVertical < 2; isVertical++){
+//         for(int i = 0; i < SIZE; i++) {
+//             for (string wrd: allWords) {
+//                 length = wrd.length();
+//                 for(int t = 0; t < (6-length); t++) {
+//                     isWord = true;
+//                     for(int j = 0+t; j < length+t; j++) {
+//                         if(field[isVertical?j:i][isVertical?i:j] == wrd[j-t] && isWord) {
+//                             isWord = true;
+//                         } else {
+//                             isWord = false;
+//                         }
+//                         if(j == SIZE-(6-length)+t && isWord) {
+//                             foundWords.insert(wrd);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
